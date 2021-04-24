3f89838bc569f2687cbb23d1e569a492391759e0
kferg357 <kennethferguson90@gmail.com>
2021-04-24 15:53:22 -0400

    added java, style and html

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,9 +1,11 @@
-var start_btn = document.querySelector(".start_btn button");
+var start_btn = document.querySelector(".start_btn_button");
 var info_area = document.querySelector(".info_area");
 var timerInterval = document.querySelector(".timer");
 var info_box = document.querySelector("info_box");
 var question_box = document.getElementById('#question_box')
 var question = document.querySelector("question")
+var answer = document.querySelector("answer")
+ var numb = document.querySelector("number")
 
 function setTime() {
     // Sets interval in variable
@@ -11,22 +13,30 @@ function setTime() {
       secondsLeft--;
       timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
 
+start_btn.addEventListener('click', function()){
+  console.log(start_btn);
+  document.querySelector(".start_btn_button").hidden=true;
+}
 
 
 
 // When start quiz button is clicked
 start_btn.onclick = ()=>{
-    info.box.classList.remove("activateInfo"): //hide info box
-    info.
+    info_box.classList.remove("activateInfo_area"); //hide info box
+    question.classList.add("activateQuestion_box"); //show question box
 }
 
 
+numb: 1,
+question:
+answer:
+choices:[
 
+]
 
-
-if(secondsLeft === 0) {
-    // Stops execution of action at set interval
-    clearInterval(timerInterval);
-    // Calls function to create and append image
-    sendMessage();
-  }
+// if(secondsLeft === 0) {
+//     // Stops execution of action at set interval
+//     clearInterval(timerInterval);
+//     // Calls function to create and append image
+//     sendMessage();
+//   }
