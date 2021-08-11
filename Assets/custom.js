5031e72286684d4312d79d11f5997971da85bf37
kferg357 <kennethferguson90@gmail.com>
2021-08-11 15:14:55 -0400

    added to custom.js

--- Assets/custom.js
+++ Assets/custom.js
@@ -6,20 +6,42 @@ var count = 0;
 var scoreCount = 0;
 var duration = 0;
 
-var qaSet = document.getElementById('.qa_set');
-var qaAnsRow = document.getElementById('.qa_set .qa_ans_row input');
+var qaSet = document.querySelectorAll('.qa_set');
+var qaAnsRow = document.querySelectorAll('.qa_set .qa_ans_row input');
 
-skip.addEventListener('click', function() {
-    step()
+skip.addEventListener('click', function () {
+    console.log('step!!!')
+    step();
 })
 
+qaAnsRow.forEach(function (qaAnsRowSingle) {
+    qaAnsRowSingle.addEventListener('click', function () {
+        setTimeout(function () {
+            step();
+        }, 500)
+
+        var valid = this.getAttribute("valid");
+        if (vaild == "valid") {
+            scoreCount += 20;
+            score.innerHTML = scoreCount;
+            totalScore.innerHTML = scoreCount;
+        } else {
+            scoreCount -= 20;
+            score.innerHTML = scoreCount;
+            totalScore.innerHTML = scoreCount;
+        }
+    })
+});
+
+
 function step() {
-    count =+ 1;
-    for(var i = 0; i < qaSet.length; i++) {
-        qaSet[i].className= 'qa_set';
+    count += 1;
+    for (var i = 0; i < qaSet.length; i++) {
+        qaSet[i].className = 'qa_set';
     }
-qaSet[count].className= 'qa_set acive';
-if(count == 5){
-    skip.style.display = 'none';
+    qaSet[count].className = 'qa_set active';
+    if (count == 10) {
+        skip.style.display = 'none';
+    }
 }
-}
+
