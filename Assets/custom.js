3a10c5714a3185722f4bcc9d6eb0a3c3aa184d32
kferg357 <kennethferguson90@gmail.com>
2021-08-12 15:49:13 -0400

    added readme

--- Assets/custom.js
+++ Assets/custom.js
@@ -2,29 +2,17 @@ var skip = document.getElementById('skip');
 var scpre = document.getElementById('score');
 var totalScore = document.getElementById('totalScore');
 var countdown = document.getElementById('countdown');
+var username = document.getElementById('username');
+var submitBtn = document.getElementById('submit');
 var count = 0;
 var scoreCount = 0;
 var duration = 0;
 
-// const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-// console.log(highScores);
-// // finsalScore.innerText = mostRecentScore;
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+// This is a JS Object
+console.log(highScores);  // --> [ { score: 90, user: "Tim"} ]
 
-// username.addEventListener("keyup", () => {
-//     saveScoreBtn.disabled = !username.value;
-// });
 
-// savedHighScore = e => {
-//     console.log("clicked the save button")
-//     e.preventDefault();
-
-//     const score = {
-//         score: mostRecentScore,
-//         name: username.value
-//     };
-//     highScores.push(score);
-//     console.log(highScores);
-// };
 
 
 var qaSet = document.querySelectorAll('.qa_set');
@@ -68,6 +56,9 @@ function step() {
         skip.style.display = 'none';
         clearInterval(durationTime);
         countdown.innerHTML = 0;
+
+        // call a function to save our data
+       // getUserData();
     }
 }
 
@@ -82,6 +73,59 @@ var durationTime = setInterval(function () {
     }
 }, 1000);
 
+
+submitBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const score = {
+        score: scoreCount,
+        name: username.value
+    }
+
+    console.log(score);
+
+    // Added new Data
+    highScores.push(score);
+
+    // Sorting the data (High Scores)
+    
+    // Add all data to our List of High Scores Container (<div>)
+
+    // 1) Clear The container out
+
+    // 2) Loop through the ARRAY
+
+    // 3) Create a new HTML ELEMENT
+        //   - Add any classes for styling
+        //   - Add CONTENT (innerContext, innerHTML - Data)
+        //   - ADD all the new ELEMENTS to the DOM
+
+
+    // Update the Local Storage Object
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+});
+
+// function getUserData() {
+
+//     // Grab the Users Score And Username and create a data object
+//     const score = {
+//         score: scoreCount,
+//         name: username.value
+//     }
+
+//     console.log(score);
+//     // Added new Data
+//     highScores.push(score);
+
+//     // Sorting the data (High Scores)
+    
+//     // Add all data to our List of High Scores Container (<div>)
+
+
+//     // Update the Local Storage Object
+//     localStorage.setItem('highScores', JSON.stringify(highScores));
+
+// }
 // savedHighScore = e => {
 //     console.log("clicked the save button")
 //     e.preventDefault();
